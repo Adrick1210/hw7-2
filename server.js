cee7f333bfad1823ec82ae942e79f04d3d3305e4
Christian Alexander <christian.alexanderato@gmail.com>
2023-12-16 08:07:31 -0800

    exported drinks data - imported drinks to server - json data viewable

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ const express = require("express");
 
 const app = express();
 
+const drinks = require("./models/drinks.js");
+
 app.get("/", (req, res) => {
-    res.send('<h1>Welcome to the Gitpub app!</h1>')
-})
+  res.send("<h1>Welcome to the Gitpub app!</h1>");
+});
+
+app.get("/drinks", (req, res) => {
+  res.send(drinks);
+});
 
 app.listen(3000, () => {
   console.log("App is running");
